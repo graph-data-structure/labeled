5f259cfbd885ec23c4ca8f4f4f3ca3678500a7c6
aureooms <aurelien.ooms@gmail.com>
2015-08-17 11:42:16 +0000

    update dependencies, add more tests

---
+++ test/js/src/000-simple/MultiGraph.js
@@ -0,0 +1,44 @@
+
+var collections = require( "aureooms-js-collections" ) ;
+var set = collections.set ;
+
+var cardinality = require( "aureooms-js-cardinality" ) ;
+
+var dll = require( "aureooms-js-dll" ) ;
+
+var spec = require( "aureooms-js-graph-spec" ) ;
+var list = require( "aureooms-js-adjacency-list" ) ;
+
+var label = graphlabeled.label ;
+var List = dll.DoublyLinkedList ;
+
+var MultiGraph = label( list.MultiGraph( List ) ) ;
+
+test( "simple test for labeled MultiGraph" , function ( assert ) {
+
+	var G = new MultiGraph( ) ;
+
+	assert.equal( cardinality.len( G.vertices( ) ) , 0 , "no vertices count" ) ;
+
+	G.vadd( 1 ) ;
+	G.vadd( 2 ) ;
+	G.vadd( 3 ) ;
+
+	assert.equal( cardinality.len( G.vertices( ) ) , 3 , "3 vertices count" ) ;
+	assert.ok( set( G.vertices( ) ).isequal( [ 1 , 2 , 3 ] ) , "3 vertices" ) ;
+
+	assert.equal( cardinality.len( G.edges( ) ) , 0 , "no edges count" ) ;
+
+	var __12 = G.eadd( 1 , 2 ) ;
+	var __23 = G.eadd( 2 , 3 ) ;
+	var _12 = G.eadd( 1 , 2 ) ;
+	var _23 = G.eadd( 2 , 3 ) ;
+
+	assert.equal( cardinality.len( G.edges( ) ) , 4 , "4 edges count" ) ;
+
+	G.vdel( 2 ) ;
+
+	assert.equal( cardinality.len( G.vertices( ) ) , 2 , "vertices after vdel 2" ) ;
+	assert.equal( cardinality.len( G.edges( ) ) , 0 , "edges after vdel 2" ) ;
+
+} ) ;
